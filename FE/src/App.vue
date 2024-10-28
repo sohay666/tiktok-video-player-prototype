bc87f7e57a1d3ed0830c1a01c5c609e3fd97963a
Sohay <sohai@kucingliar.xyz>
2024-10-28 16:26:39 +0700

    New: Integrated FE - implement responsive design for mobile view - integrated with calling api

--- FE/src/App.vue
+++ FE/src/App.vue
@@ -1,8 +1,23 @@
 <template>
-  <h1>Hello</h1>
+  <div id="app">
+    <VideoList />
+  </div>
 </template>
+
+<script>
+import VideoList from './components/VideoList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    VideoList,
+  },
+};
+</script>
 
 <style>
 @import './assets/base.css';
+/* #app{
+  background: black;
+} */
 </style>
-
